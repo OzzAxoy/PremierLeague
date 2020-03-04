052cd76ffe3a6a25da92df0b82070c57d8ba28e8
OguzAksoy <ozzaxoy@gmail.com>
2020-03-04 22:30:23 +0300

    Add project files.

---
+++ PremierLeague/cMain.cpp
@@ -0,0 +1,15 @@
+#include "cMain.h"
+
+cMain::cMain() : wxFrame(   nullptr,
+                            wxID_ANY, 
+                            "Premier League 2019-2020",
+                            wxPoint(100,100), wxSize(1280, 720)
+                        )
+{
+    
+}
+
+cMain::~cMain()
+{
+
+}
