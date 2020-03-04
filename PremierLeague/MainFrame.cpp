df569114a6f0ba39e0f0307e566b332881d860b2
OguzAksoy <ozzaxoy@gmail.com>
2020-03-04 23:48:57 +0300

    ChangedMainFrameStyle

--- PremierLeague/MainFrame.cpp
+++ PremierLeague/MainFrame.cpp
@@ -3,7 +3,8 @@
 MainFrame::MainFrame() : wxMDIParentFrame ( nullptr,
                                   wxID_ANY,
                                   "Premier League 2019-2020",
-                                  wxPoint(100, 100), wxSize(1280, 720)
+                                  wxPoint(100, 100), wxSize(1280, 720),
+                                  wxDEFAULT_FRAME_STYLE
                                 )
 {
 
